64b6c997a769560062460b61f378b5980d341564
Inayet Hadi <inayet@dreamsapi.com>
2025-08-18 23:04:35 -0600

    adjsuted flake.nix to use flakelight file for improvements

--- flake.nix
+++ flake.nix
@@ -9,7 +9,8 @@
   # --- CORRECTED LINE ---
   # `outputs` is now assigned directly to `mkFlake`.
   # flakelight handles creating the function that receives the inputs.
-  outputs = flakelight.lib.mkFlake {
+  outputs = {flakelight,nixpkgs,...}:
+   flakelight.lib.mkFlake {
     # 1. Tell flakelight where to get packages for each system
     # `nixpkgs` is an input and is automatically available here.
     pkgs = nixpkgs.legacyPackages;
