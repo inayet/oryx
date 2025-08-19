9ddec25fd78998add1533f29b428ad516fbcdc77
Inayet Hadi <inayet@dreamsapi.com>
2025-08-18 19:49:13 -0600

    README.md

--- flake.nix
+++ flake.nix
@@ -8,40 +8,42 @@
   };
 
   # Define flake outputs
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    # Define a package for the default system (x86_64-linux)
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation rec {
-      pname = "oryx";
-      version = "0.6.1";
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    {
+      # Define a package for the default system (x86_64-linux)
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation rec {
+        pname = "oryx";
+        version = "0.6.1";
 
-      # Download the pre-built ORYX binary from the GitHub release
-      src = nixpkgs.legacyPackages.x86_64-linux.fetchurl {
-        url = "https://github.com/pythops/oryx/releases/download/v${version}/oryx-x86_64-unknown-linux-musl";
-        # You MUST replace the following with the actual SHA256 hash of the binary file
-        # To get the SHA256 hash, download the file and run `sha256sum oryx-x86_64-unknown-linux-musl`
-        sha256 = "sha256-R6M43qLjiMFDgseGQrIGzTsdWO/wqt1/bgIFMGS+yTc=";
-      };
+        # Download the pre-built ORYX binary from the GitHub release
+        src = nixpkgs.legacyPackages.x86_64-linux.fetchurl {
+          url = "https://github.com/pythops/oryx/releases/download/v${version}/oryx-x86_64-unknown-linux-musl";
+          # You MUST replace the following with the actual SHA256 hash of the binary file
+          # To get the SHA256 hash, download the file and run `sha256sum oryx-x86_64-unknown-linux-musl`
+          sha256 = "sha256-R6M43qLjiMFDgseGQrIGzTsdWO/wqt1/bgIFMGS+yTc=";
+        };
 
-      # Add this line to skip the unpack phase
-      dontUnpack = true;
+        # Add this line to skip the unpack phase
+        dontBuild = true;
 
-      # Installation phase
-      installPhase = ''
-        mkdir -p $out/bin
-        cp $src $out/bin/oryx
-        chmod +x $out/bin/oryx
-      '';
+        # Installation phase
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src $out/bin/oryx
+          chmod +x $out/bin/oryx
+        '';
 
-      # Additional information (optional)
-      meta = with nixpkgs.legacyPackages.x86_64-linux.lib; {
-        description = "TUI for sniffing network traffic";
-        homepage = "https://github.com/pythops/oryx";
-        license = licenses.mit;
-        platforms = platforms.linux;
+        # Additional information (optional)
+        meta = with nixpkgs.legacyPackages.x86_64-linux.lib; {
+          description = "TUI for sniffing network traffic";
+          homepage = "https://github.com/pythops/oryx";
+          license = licenses.mit;
+          platforms = platforms.linux;
+        };
       };
     };
-  };
 }
