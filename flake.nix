2cc8e8ebe46523a8bd63c3a28fc98c9d8bdef779
Inayet Hadi <inayet@dreamsapi.com>
2025-08-18 23:20:44 -0600

    adjsuted flake.nix to use flakelight file for improvements

--- flake.nix
+++ flake.nix
@@ -6,15 +6,18 @@
     flakelight.url = "github:nix-community/flakelight";
   };
 
-  # --- CORRECTED STRUCTURE ---
-  # This more explicit function signature is a robust way to ensure
-  # all inputs are correctly passed and in scope.
-  outputs = { self, nixpkgs, flakelight, ... }:
-    flakelight.lib.mkFlake {
-      # 1. Pass the necessary inputs to flakelight.
-      inherit self nixpkgs;
+  # The `outputs` function is given all the inputs as arguments.
+  # The `@inputs` pattern captures all of them into a single attrset called `inputs`.
+  outputs = { self, ... }@inputs:
+    # `mkFlake` is the main helper from flakelight.
+    # It takes a configuration set and returns the final flake outputs.
+    inputs.flakelight.lib.mkFlake {
+      # FIX: Pass the `self` and the entire `inputs` set to flakelight.
+      # It needs this to access nixpkgs and other inputs correctly.
+      inherit self inputs;
 
-      # 2. Define the 'oryx' package.
+      # Define the 'oryx' package for each system.
+      # flakelight automatically passes `pkgs` for the correct system here.
       oryx = { pkgs, ... }: pkgs.rustPlatform.buildRustPackage rec {
         pname = "oryx";
         version = "0.6.1";
@@ -23,14 +26,16 @@
           owner = "pythops";
           repo = "oryx";
           rev = "v${version}";
-          # This is a placeholder hash. You still need to get the correct one.
+          # NOTE: This is a placeholder hash. The first time you build,
+          # Nix will fail and tell you the correct hash to put here.
           hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         };
 
-        # This is also a placeholder hash you need to find.
+        # NOTE: This is also a placeholder. The build will fail and provide
+        # the correct hash for you to copy here.
         cargoSha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
 
-        # System libraries needed for building and running
+        # System libraries needed for building and running.
         nativeBuildInputs = [ pkgs.pkg-config ];
         buildInputs = [ pkgs.libpcap ];
 
@@ -46,20 +51,21 @@
         };
       };
 
-      # 3. Set the default package for `nix build` and `nix run`.
-      default = { self, ... }: self.packages.oryx;
+      # Set the default package for `nix build` and `nix run`.
+      # `self'` here refers to the final, evaluated flake outputs for a given system.
+      default = { self', ... }: self'.packages.oryx;
 
-      # 4. Define the development shell.
-      devShells.default = { pkgs, self, ... }: pkgs.mkShell {
+      # Define the development shell.
+      devShells.default = { pkgs, self', ... }: pkgs.mkShell {
         # Inherit all the build dependencies from the `oryx` package.
-        inputsFrom = [ self.packages.oryx ];
+        inputsFrom = [ self'.packages.oryx ];
         # Add any extra tools you want for development.
         packages = with pkgs; [
           rust-analyzer # For better IDE support
         ];
       };
 
-      # 5. Define the formatter for `nix fmt`.
+      # Define the formatter for `nix fmt`.
       formatter = { pkgs, ... }: pkgs.nixpkgs-fmt;
     };
 }
